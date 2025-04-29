e63007d96e87c82f4136767d5dee12976ccf0143
Mohammad Shoubash <mohammedshobash2002@gmail.com>
2025-04-29 14:13:17 +0300

    add categories page

--- frontend-ecommerce/src/App.js
+++ frontend-ecommerce/src/App.js
@@ -1,27 +1,11 @@
-// App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Categories from "./components/Categories";
 
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Checkout from './pages/Checkout';
-import Categories from './pages/Categories';
-import Cart from './pages/Cart';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </Router>
+    <div>
+      <Categories />
+    </div>
   );
-};
+}
 
 export default App;
